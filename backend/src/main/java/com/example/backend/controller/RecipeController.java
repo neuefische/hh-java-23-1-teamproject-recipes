a30ed790512816af0429a59f76cfb7faabc8c3c8
lukas-kraus <lukaskraus@me.com>
2023-03-31 16:34:59 +0200

    Implemented axios, changed Spring dependency

--- backend/src/main/java/com/example/backend/controller/RecipeController.java
+++ backend/src/main/java/com/example/backend/controller/RecipeController.java
@@ -5,6 +5,7 @@ import com.example.backend.model.Recipe;
 import com.example.backend.service.RecipeService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +19,14 @@ public class RecipeController {
 
     private final RecipeService recipeService;
 
-    @GetMapping("recipes")
+    @GetMapping("/recipes")
     List<Recipe> getAllRecipes() {
         return recipeService.getAllRecipes();
     }
 
+    @GetMapping("/test")
+    public String testMethode() {
+        return "Teststring";
+    }
 
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    axios.get("/api/test")
+        .then(response => {
+          setData(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+  }, []);
   return (
       <BrowserRouter>
     <div className="App">
+      <h1>Testhead</h1>
+      <p>{data}</p>
      {/* <Routes>
         <Route>
 
