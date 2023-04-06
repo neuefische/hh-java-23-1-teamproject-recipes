fb6e292339139284e681ce665dd26e09c2ff7d9b
bolabolor <bolaskekse@gmail.com>
2023-04-06 18:01:10 +0200

    added RecipeDetail

---
+++ frontend/src/components/RecipeDetail.tsx
@@ -0,0 +1,45 @@
+import React, {ReactNode, useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import axios from "axios";
+import {Recipe} from "../model/Recipe";
+
+
+function Typography(props: { children: ReactNode }) {
+    return null;
+}
+
+export default function TodoDetail() {
+
+    const [recipe, setRecipe] = useState<Recipe>()
+    const {id} = useParams<{ id: string }>()
+
+    useEffect(() => {
+        if (id) {
+            loadRecipeById(id)
+        }
+    }, [])
+
+    function loadRecipeById(id: string) {
+        axios.get('/api/todo/' + id)
+            .then((response) => {
+                setRecipe(response.data)
+            })
+            .catch((error) => {
+                console.error("Recipe not found")
+            })
+    }
+    return (
+        <div>
+            {
+                recipe
+                    ? <div>
+                        <Typography>{recipe.id}</Typography>
+                        <Typography>{recipe.name}</Typography>
+                        <Typography>{recipe.category}</Typography>
+                    </div>
+                    : <div>Loading...</div>
+            }
+
+        </div>
+    )
+}
