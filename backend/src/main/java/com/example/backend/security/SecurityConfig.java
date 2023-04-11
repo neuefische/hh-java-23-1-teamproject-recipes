c44da54fd0dea96934987fc2778fdc2ec565bd38
junesdream <norisori@gmail.com>
2023-04-11 12:59:33 +0200

    Added encoder for password

--- backend/src/main/java/com/example/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -28,8 +29,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    public PasswordEncoder passwordEncoder(){
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 
 }
