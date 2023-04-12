4407497584e6e13432c7007dc2078cd388f6c02f
bolabolor <bolaskekse@gmail.com>
2023-04-12 17:46:47 +0200

    added RecipeDetail

--- frontend/src/components/AddRecipe.tsx
+++ frontend/src/components/AddRecipe.tsx
@@ -1,8 +1,5 @@
 import {FormEvent, useState} from "react";
-import {NewRecipe, Recipe} from "../model/Recipe";
-import axios from "axios";
 import "./AddRecipe.css"
-import {useNavigate} from "react-router-dom";
 
 type AddRecipeProps = {
     addRecipe: (newRecipe: string) => void
@@ -12,20 +9,8 @@ export default function AddRecipe(props: AddRecipeProps) {
     const initialState: string = ""
     const [name, setName] = useState<string>(initialState);
 
-    const navigate = useNavigate();
-
     function onSaveRecipe(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        // const newRecipe: NewRecipe = {name: 'Recipe Name', category: "ASIAN"};
-        /*axios
-            .post('/api/recipes', newRecipe)
-            .then(response => {
-                const recipe: Recipe = {...response.data, id: response.data.id};
-                props.addRecipe(recipe);
-                setName('');
-                navigate('/recipes');
-            })
-            .catch(console.error);*/
         props.addRecipe(name);
     }
 
