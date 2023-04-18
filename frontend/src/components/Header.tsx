276b2607e9bc6831f0ef3c1faa0b2be9d21cf292
bolabolor <bolaskekse@gmail.com>
2023-04-18 17:09:44 +0200

    changed small spellingerrors

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,11 +6,9 @@ export default function Header(){
     return(
         <header className="header">
             <h1 className="header_title">Daily Recipes</h1>
-
             <Link className="headerLink" to='/recipe'>Recipes</Link>
             <NavLink className="headerNav" to='/recipes/add'>ADD</NavLink>
             <NavLink className="headerNav" to='/login'>Login</NavLink>
-
         </header>
     )
 }
--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -14,6 +14,7 @@ export default function RecipeCard(props: Props){
             <p>{props.recipe.name}</p>
             <p>{props.recipe.category}</p>
           <button onClick={() => {navigate('/recipes/' + props.recipe.id)}}>Detail</button>
+          <button onClick={() => {navigate('/recipes/update/' + props.recipe.id)}}>Update</button>
         </div>
     )
 }
